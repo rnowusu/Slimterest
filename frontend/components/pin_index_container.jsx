9e1b738689a8673a532ebe7789abb6a1664de70c
Ryan <rnowusu@gwu.edu>
2018-06-01 15:45:32 -0400

    added styling, added shuffle to pin index

--- frontend/components/pin_index_container.jsx
+++ frontend/components/pin_index_container.jsx
@@ -4,9 +4,17 @@ import PinIndex from './pin_index';
 import { openItemModal } from '../actions/item_modal_actions';
 import { openMenu, closeMenu } from '../actions/click_menu_actions';
 
+const shuffle = (a) => {
+for (let i = a.length - 1; i > 0; i--) {
+        const j = Math.floor(Math.random() * (i + 1));
+        [a[i], a[j]] = [a[j], a[i]];
+    }
+    return a;
+  }
+
 const mapStateToProps = (state) => {
   return {
-    pins: Object.values(state.entities.pins),
+    pins: shuffle(Object.values(state.entities.pins)),
     menuType: state.ui.menu
   };
 };
